be007608fbd571dd8126b0c9f2f25a7a729a751c
Rahmad Hidayat <inbox.rahmad@gmail.com>
2023-08-03 15:37:57 +0700

    chore: update dependencies to the latest version

---
+++ app/build.gradle.kts
@@ -0,0 +1,68 @@
+import java.io.FileInputStream
+import java.util.*
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.rahmad.popularmoviesstage2"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.rahmad.popularmoviesstage2"
+        minSdk = 21
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        // Load local.properties
+        val localPropertiesFile = rootProject.file("local.properties")
+        val localProperties = Properties()
+        localProperties.load(FileInputStream(localPropertiesFile))
+
+        buildConfigField("String", "API_KEY", localProperties["API_KEY"].toString())
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+    productFlavors {
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("include" to listOf("*.jar"), "dir" to "libs")))
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.1.0") {
+        exclude(group = "com.android.support", module = "support-annotations")
+    }
+    implementation("androidx.appcompat:appcompat:1.0.0")
+    implementation("com.google.android.material:material:1.0.0")
+    implementation("androidx.recyclerview:recyclerview:1.0.0")
+    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+    implementation("com.squareup.picasso:picasso:2.5.2")
+    implementation("com.google.code.gson:gson:2.7")
+    implementation("com.squareup.retrofit2:retrofit:2.3.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.3.0")
+    implementation("com.github.jd-alexander:LikeButton:0.2.3")
+    implementation("com.facebook.stetho:stetho:1.1.1")
+    testImplementation("junit:junit:4.12")
+}
